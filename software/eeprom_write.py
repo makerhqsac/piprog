220be0d1807200c44cacd242cb1422dcb14c29cb
Mike Machado <mike@machadolab.com>
2016-10-26 16:26:34 -0700

    Working eeprom writer

--- software/eeprom_write.py
+++ software/eeprom_write.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from subprocess import call
+import subprocess
 import sys
 import os
 import time
@@ -9,7 +9,7 @@ import time
 # Path to binary eeprom.epp file to write to eeprom.
 # Use the eepmake command from https://github.com/raspberrypi/hats/eepromutils to create this file
 # based on the information from eeprom_settings.txt
-EEPROM_FILE = "eeprom.epp"
+EEPROM_FILE = "/path/to/eeprom.eep"
 
 # Type of EEPROM. Supported types are "24c32", "24c64", "24c128", "24c512", "24c1024".
 EEPROM_TYPE = "24c32"
@@ -20,13 +20,15 @@ EEPROM_TYPE = "24c32"
 
 ### main code ##
 
-PIN_LED_GREEN = 13
-PIN_LED_RED = 19
-PIN_BUTTON = 26
+PIN_LED_GREEN = 33
+PIN_LED_RED = 35
+PIN_BUTTON = 37
 
-I2C_DEV_PATH = "/sys/class/i2c-adapter/i2c-3/3-0050"
+I2C_DEV_PATH = "/sys/class/i2c-adapter/i2c-3"
+I2C_DEV_NAME = "3-0050"
 I2C_USE_BITBANG = True
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(PIN_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -35,32 +37,35 @@ GPIO.setup(PIN_LED_RED, GPIO.OUT, initial=GPIO.LOW)
 
 
 def main():
+    print("eeprom writer running - press button to write eeprom!")
     setup_i2c()
     while True:
         GPIO.output(PIN_LED_GREEN, GPIO.HIGH)
         if GPIO.input(PIN_BUTTON) == GPIO.LOW:
+            print("writing eeprom... ")
             GPIO.output(PIN_LED_GREEN, GPIO.LOW)
             write_eeprom()
+            print("   DONE!")
     GPIO.cleanup()
 
 
 def setup_i2c():
-    if not call(["modprobe", "i2c_dev"]) == 0:
+    if not subprocess.call(["modprobe", "i2c_dev"]) == 0:
         print("error loading i2c_dev kernel module")
         exit(1)
     if I2C_USE_BITBANG:
-        if not call(["dtoverlay", "i2c-gpio","i2c_gpio_sda=2","i2c_gpio_scl=3"]) == 0:
+        if not subprocess.call(["dtoverlay", "i2c-gpio","i2c_gpio_sda=2","i2c_gpio_scl=3"]) == 0:
             print("error loading i2c-gpio dtoverlay. If the dtoverlay command is not found, please update to latest raspian")
             exit(1)
-    if not call(["modprobe","at24"]) == 0:
+    if not subprocess.call(["modprobe","at24"]) == 0:
         print("error loading at24 kernel module")
         exit(1)
-    if not os.path.exists(I2C_DEV_PATH):
+    if not os.path.exists(I2C_DEV_PATH+"/"+I2C_DEV_NAME):
         new_device = open(I2C_DEV_PATH+"/new_device", "w")
         new_device.write(EEPROM_TYPE+" 0x50\n")
         new_device.close()
-    if not os.path.exists(I2C_DEV_PATH+"/eeprom"):
-        print("eeprom does not exist on i2c device " + I2C_DEV_PATH)
+    if not os.path.exists(I2C_DEV_PATH+"/"+I2C_DEV_NAME+"/eeprom"):
+        print("eeprom does not exist on i2c device " + I2C_DEV_PATH+"/"+I2C_DEV_NAME)
         exit(1)
 
 
@@ -69,6 +74,7 @@ def blink_led(pin, times=1, delay=0.5):
         GPIO.output(pin, GPIO.HIGH)
         time.sleep(delay)
         GPIO.output(pin, GPIO.LOW)
+        time.sleep(delay)
 
 
 def write_eeprom():
@@ -78,7 +84,8 @@ def write_eeprom():
         print("eeprom file "+EEPROM_FILE+" does not exist!")
         blink_led(PIN_LED_RED, 3, 0.6)
     else:
-        result = call(["dd", "if="+EEPROM_FILE, "of="+I2C_DEV_PATH+"/eeprom"])
+        FNULL = open(os.devnull, 'w')
+        result = subprocess.call(["dd", "if="+EEPROM_FILE, "of="+I2C_DEV_PATH+"/"+I2C_DEV_NAME+"/eeprom"], stdout=FNULL, stderr=subprocess.STDOUT)
 
         GPIO.output(PIN_LED_GREEN, GPIO.LOW)
         GPIO.output(PIN_LED_RED, GPIO.LOW)
@@ -88,7 +95,7 @@ def write_eeprom():
             blink_led(PIN_LED_GREEN, 3, 0.2)
         else:
             print("error writing eeprom")
-            blink_led(PIN_LED_RED, 3, 0.6)
+            blink_led(PIN_LED_RED, 3, 0.5)
 
 
 
